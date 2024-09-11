e11ac08fea13ee9becb8e7161a615fc403e4fc11
win <wwinnn478@gmail.com>
2024-09-11 18:37:20 +0700

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ import {
   Title,
 } from "@mantine/core";
 
-import { Form, useForm, zodResolver } from "@mantine/form";
+import { useForm, zodResolver } from "@mantine/form";
 import { z } from "zod";
 
 const schema = z
@@ -48,7 +48,8 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z.string().min(6, {message: 'Password must be at least 6 characters'}
+    ).max(12, {message: 'Password must not exceed 12 characters'}),
     confirmPassword: z.string(),
   })
   .refine(
@@ -68,6 +69,15 @@ const schema = z
     {
       message: "Invalid coupon code",
       path: ["coupon"],
+    }
+  )
+  .refine (
+    (data) => {
+      if (data.password === data.confirmPassword) return true;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
     }
   );
 
@@ -98,9 +108,11 @@ export default function Home() {
     if (form.values.plan === "funrun") price = 500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
-
+    if(form.values.plan === "mini") price = 800;
+    if(form.values.plan === "half") price = 1200;
+    if(form.values.plan === "full") price = 1500;
     //check discount here
-
+    if(form.values.hasCoupon&&form.values.coupon === "CMU2023") price = price-(price*(30/100));
     return price;
   };
 
@@ -187,10 +199,12 @@ export default function Home() {
           </Stack>
         </form>
 
-        {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+        <Footer year={2024} fullName="Chanachai Chamnanmor" studentId="660612140" /> 
+              
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
+      
     </div>
   );
 }
