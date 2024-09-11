ad6d3d42f1c7204d71cda03e09b8be4f266728cb
win <wwinnn478@gmail.com>
2024-09-11 18:46:29 +0700

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,8 +48,7 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string().min(6, {message: 'Password must be at least 6 characters'}
-    ).max(12, {message: 'Password must not exceed 12 characters'}),
+    password: z.string(),
     confirmPassword: z.string(),
   })
   .refine(
@@ -76,10 +75,10 @@ const schema = z
       if (data.password === data.confirmPassword) return true;
     },
     {
-      message: "Password does not match",
+      message: "“Password does not match",
       path: ["confirmPassword"],
     }
-  );
+    )
 
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -108,11 +107,12 @@ export default function Home() {
     if (form.values.plan === "funrun") price = 500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
-    if(form.values.plan === "mini") price = 800;
-    if(form.values.plan === "half") price = 1200;
-    if(form.values.plan === "full") price = 1500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "mini") price = 1200;
+    if (form.values.plan === "mini") price = 1500;
+
     //check discount here
-    if(form.values.hasCoupon&&form.values.coupon === "CMU2023") price = price-(price*(30/100));
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023") return true;
     return price;
   };
 
@@ -126,7 +126,7 @@ export default function Home() {
         <Space h="lg" />
 
         {/* add form */}
-        <form onSubmit={form.onSubmit((v) => alert("See you at CMU Marathon"))}>
+        <form onSubmit={form.onSubmit(() => alert("See you at CMU Marathon"))}>
           <Stack gap="sm">
             <Group grow align="start">
               <TextInput
@@ -199,12 +199,10 @@ export default function Home() {
           </Stack>
         </form>
 
-        <Footer year={2024} fullName="Chanachai Chamnanmor" studentId="660612140" /> 
-              
+        <Footer year={2024} fullName="Chanachai Chamnanmor" studentId="660612140" />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
-      
     </div>
   );
 }
